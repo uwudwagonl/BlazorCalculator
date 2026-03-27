67f392920944d24cc439dfe1becf4770fa2f7008
S.Schoegl <S.Schoegl@E1C03>
2026-03-27 11:04:29 +0100

    Whack A mole

---
+++ BlazorCalculator/Components/Pages/Whackamole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorCalculator.Components.Pages
+{
+    public class WhackamoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
